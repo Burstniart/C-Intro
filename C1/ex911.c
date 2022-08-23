74e3096222ae0d5e1bc9f4463f795c512e7f772d
Burstniart <v4gr33n@hotmail.com>
2022-08-22 21:02:17 -0500

    Using arrays now

---
+++ C1/ex911.c
@@ -0,0 +1,13 @@
+/* Exercise 1-9. How would you test the word count program? What are some boundaries?
+If a single word is an array of characters that are separated by blanks or new lines count those
+
+Exercise 1-10. Write a program which prints the words in its input, one per line.
+
+
+Exercise 1-11. Revise the word count program to use a better definition of "word," 
+for example, a sequence of letters, digits and apostrophes that begins with a letter. */
+
+#include <stdio.h>
+main() {
+
+}
