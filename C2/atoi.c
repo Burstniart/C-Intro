cb8986626c61e058b448210d4fb07ad8c25facf3
Burstniart <v4gr33n@hotmail.com>
2022-10-03 23:58:40 -0500

    Daily commit

--- C2/atoi.c
+++ C2/atoi.c
@@ -1,42 +1,20 @@
 #include <stdio.h>
 
-#define MAXLINE 1000
+#define TAB_LENGTH 4 
 
 main() {
-    int len, max;
-    char line[MAXLINE], save[MAXLINE];
+	int c;
+	unsigned int noSpaces;
 
-    max = 0;
-    while ((len = get_line(line, MAXLINE)) > 0)
-	    if (len > max) {
-	    	max = len;
-		copy(line, save);
-	    }
-    if (max > 0)
-	    printf("%s", save);
-}
+	while ((c=getchar()) != EOF) {
+		if (c == '\t') {
+			noSpaces = TAB_LENGTH;
 
-get_line(s, lim)
-char s[];
-int lim;
-{
-    int c, i;
-    for (i=0; i<lim-1 && (c=getchar()) != EOF && c!='\n'; ++i)
-	    s[i] = c;
-    if (c == '\n') {
-	    s[i] = c;
-    	    ++i;
-    }
-    s[i] = '\0';
-    return(i);
-}
-
-copy(s1, s2)
-char s1[], s2[];
-{
-    int i;
-
-    i = 0;
-    while ((s2[i] = s1[i]) != '\0')
-	    ++i;
+			while (noSpaces) {
+				putchar(' ');
+				--noSpaces;
+			}
+		}
+		else { putchar(c); }
+	}
 }
